967c1db012620316e794859a9173c3f8fc480752
Dipan Sutradhar <dipan.cs@gmail.com>
2023-11-15 16:13:10 -0500

    order service container impl

--- infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/KafkaConsumer.java
+++ infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/KafkaConsumer.java
@@ -5,5 +5,5 @@ import org.apache.avro.specific.SpecificRecordBase;
 import java.util.List;
 
 public interface KafkaConsumer<T extends SpecificRecordBase> {
-    void recieve(List<T> message, List<Long> keys, List<Integer> partitions, List<Long> offsets);
+    void recieve(List<T> messages, List<String> keys, List<Integer> partitions, List<Long> offsets);
 }
